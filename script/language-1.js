97f46104676fec7e3c0dd480b1cc1e0531c60ebb
moroni <carlosmoronisud@gmail.com>
2024-05-25 01:23:12 -0300

    deleted lang1

--- script/language-1.js
+++
@@ -1,6 +0,0 @@
-function texto () {
-    document.getElementById("text").innerHTML = "Hello World";
-}
-document.getElementById('port').EventListener('click', () => changeLanguage('pt'));
-document.getElementById('es').addEventListener('click', () => changeLanguage('es'));
-document.getElementById('en').addEventListener('click', () => changeLanguage('en'));
