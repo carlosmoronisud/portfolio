6533d4a2ebd8d2bde246987e4b34de4aceb0b751
moroni <carlosmoronisud@gmail.com>
2024-10-03 16:10:26 -0300

    total

---
+++ script/sanduba.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const navContent = document.querySelector('.navContent');
+    navContent.classList.toggle('active');
+});
